213e3d37cf06fe4b181d60b5d28328997934f9a5
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2021-01-24 11:36:04 +0200

    Labs, Exercises and Exams

---
+++ C_Sharp/CSharp Fundamentals/FinalExams/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = a++;
+            int c = ++a;
+
+            Console.WriteLine(c);
+
+        }
+    }
+}
