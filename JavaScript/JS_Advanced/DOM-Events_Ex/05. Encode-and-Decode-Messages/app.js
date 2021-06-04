6f40a7feb64ca725b0fd052d2ca06dc55a3e4819
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2021-06-04 17:09:47 +0300

    Ex

--- JavaScript/JS_Advanced/DOM-Events_Ex/05. Encode-and-Decode-Messages/app.js
+++ JavaScript/JS_Advanced/DOM-Events_Ex/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,68 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+
+
+    let encodeBtn = document.getElementsByTagName('button')[0];
+    let decodeBtn = document.getElementsByTagName('button')[1];
+
+    encodeBtn.addEventListener('click', onClickEncode);
+    decodeBtn.addEventListener('click', onClickDecode);
+
+    function onClickEncode(event) {
+
+        let messageToEncode = document.getElementsByTagName('textarea')[0];
+
+        let result = document.getElementsByTagName('textarea')[1];
+
+        let encodedMessage = encode(messageToEncode.value);
+
+        result.value = encodedMessage;
+        messageToEncode.value = '';
+
+    }
+
+    function onClickDecode(event) {
+
+        let messageToDecode = document.getElementsByTagName('textarea')[1];
+        let result = document.getElementsByTagName('textarea')[0];
+
+        let decodedMessage = decode(messageToDecode.value);
+
+
+        result.value = decodedMessage;
+        messageToDecode.value = '';
+
+    }
+
+
+    function encode(msg) {
+
+        let newMsg = '';
+
+        for (let i = 0; i < msg.length; i++) {
+
+            let char = msg.charCodeAt(i);
+
+            newMsg += String.fromCharCode(++char);
+        }
+
+        return newMsg;
+    }
+
+    function decode(msg) {
+
+
+        let newMsg = '';
+
+        for (let i = 0; i < msg.length; i++) {
+
+            let char = msg.charCodeAt(i);
+
+            newMsg += String.fromCharCode(--char);
+        }
+
+        return newMsg;
+
+    }
+
+
 }
