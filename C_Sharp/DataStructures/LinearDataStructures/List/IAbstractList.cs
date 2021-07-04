0ba5c9c0af9bb4d40908328f651c19e712909bd9
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2021-07-04 15:27:36 +0300

    Lab

---
+++ C_Sharp/DataStructures/LinearDataStructures/List/IAbstractList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List
+{
+    interface IAbstractList<T> : IEnumerable<T>
+    {
+        int Count { get; }
+
+        T this[int index] { get; set; }
+
+        void Add(T element);
+
+        void Insert(int index, T element);
+
+        bool Contains(T element);
+
+        int IndexOf(T element);
+
+        bool Remove(T element);
+
+        void RemoveAt(int index);
+    }
+}
--- C_Sharp/DataStructures/LinearDataStructures/List/ImplementedList.cs
+++ C_Sharp/DataStructures/LinearDataStructures/List/ImplementedList.cs
@@ -1,32 +1,38 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace List
 {
-    class ImplementedList<T>
+    class ImplementedList<T> : IAbstractList<T>
     {
-        private T[] array;
-        private int index = 0;
-
+        private T[] _array;
 
 
         public ImplementedList(int initialCapacity = 4)
         {
-            array = new T[initialCapacity];
+
+            if (initialCapacity < 0)
+            {
+                throw new ArgumentOutOfRangeException
+                   ("Invalid capacity");
+            }
+
+            _array = new T[initialCapacity];
         }
 
 
-        public int Count { get => index; } 
-        public int InternalArrayCount { get => array.Length; }
+        public int Count { get; set; }
+        public int InternalArrayCount { get => _array.Length; }
 
 
         //indexer
         public T this[int i]
         {
-            get => array[i];
+            get => _array[i];
 
-            set => array[i] = value;
+            set => _array[i] = value;
         }
 
 
@@ -35,13 +41,35 @@ namespace List
 
         public void Add(T element)
         {
-            if (index == array.Length)
+            if (this.Count == _array.Length)
             {
-                array = Grow(array);
+                _array = Grow(_array);
             }
 
-            array[index] = element;
-            index++;
+            _array[Count] = element;
+            Count++;
+        }
+
+
+        public void Insert(int index, T element)
+        {
+            this.ValidateIndex(index);
+
+            if (this.Count == _array.Length)
+            {
+                _array = Grow(_array);
+            }
+
+
+            for (int i = _array.Length - 1; i > index; i--)
+            {
+                _array[i] = _array[i - 1];
+            }
+
+            _array[index] = element;
+
+            Count++;
+
         }
 
 
@@ -56,6 +84,89 @@ namespace List
             }
 
             return newArray;
+        }
+
+        public bool Contains(T element)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i].Equals(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int IndexOf(T element)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i].Equals(element))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public bool Remove(T element)
+        {
+            int indexToRemove = this.IndexOf(element);
+
+            if (indexToRemove == -1)
+            {
+                return false;
+            }
+
+            for (int i = indexToRemove; i < this.Count - 1; i++)
+            {
+                _array[i] = _array[i + 1];
+            }
+
+            _array[Count] = default;
+
+            Count--;
+
+            return true;
+
+        }
+
+        public void RemoveAt(int index)
+        {
+            this.ValidateIndex(index);
+
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                _array[i] = _array[i + 1];
+            }
+
+            _array[Count] = default;
+
+            Count--;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return _array[i];
+
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+        
+
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index > this.Count)
+            {
+                throw new IndexOutOfRangeException("Invalid index");
+            }
         }
     }
 }
