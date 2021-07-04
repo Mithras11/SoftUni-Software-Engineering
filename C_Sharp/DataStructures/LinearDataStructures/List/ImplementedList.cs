9d1e5d7f2e9e6e460da8ec58c55e19a035f0e7dc
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2021-07-04 12:54:07 +0300

    Lab

---
+++ C_Sharp/DataStructures/LinearDataStructures/List/ImplementedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List
+{
+    class ImplementedList<T>
+    {
+        private T[] array;
+        private int index = 0;
+
+
+
+        public ImplementedList(int initialCapacity = 4)
+        {
+            array = new T[initialCapacity];
+        }
+
+
+        public int Count { get => index; } 
+        public int InternalArrayCount { get => array.Length; }
+
+
+        //indexer
+        public T this[int i]
+        {
+            get => array[i];
+
+            set => array[i] = value;
+        }
+
+
+
+
+
+        public void Add(T element)
+        {
+            if (index == array.Length)
+            {
+                array = Grow(array);
+            }
+
+            array[index] = element;
+            index++;
+        }
+
+
+
+        private T[] Grow(T[] array)
+        {
+            T[] newArray = new T[array.Length * 2];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            return newArray;
+        }
+    }
+}
---
+++ C_Sharp/DataStructures/LinearDataStructures/List/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ImplementedList<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i * 5);
+            }
+
+            Console.WriteLine(list.InternalArrayCount);
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list[5]);
+
+        }
+    }
+}
